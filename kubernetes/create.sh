1341b22157d4437acea52935f333ba08a8739d7c
suniljaswal <nothingelse007@gmail.com>
2022-08-27 18:14:22 +0530

    das

--- kubernetes/create.sh
+++ kubernetes/create.sh
@@ -1,5 +1,7 @@
 kubectl delete -f web-deployment.yaml
 kubectl delete svc web
+Kubectl delete -f db-deployment.yml
 
 kubectl create -f web-deployment.yaml
+kubectl create -f db-deployment.yml
 ./svc.sh
